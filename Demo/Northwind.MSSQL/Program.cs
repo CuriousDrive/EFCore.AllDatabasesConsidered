05064bb4d732bc28558a8bc508f3320f6a5dfb06
Curious Drive <CuriousDrive@users.noreply.github.com>
2021-11-16 04:09:27 -0500

    Renaming the folder

---
+++ Demo/Northwind.MSSQL/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// I want to change something here but it's not getting recorded
+
+builder.Services.AddDbContext<Northwind.MSSQL.Data.NorthwindContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("NorthwindDB"));
+    });
+
+builder.Services.AddMvc(option => option.EnableEndpointRouting = false)
+    .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
